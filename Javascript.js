0b78adca632016825d2dbb3525f6029e4fb9a6f5
Eduardo Olmedo <eduardolmedi00@gmail.com>
2021-05-24 13:25:30 -0600

    send changes

--- Javascript.js
+++ Javascript.js
@@ -11,3 +11,12 @@ function buttonclick()
         menuList.className = "menuOff";    
     }    
 }
+
+
+function hipotenusa(num1, num2){
+   let resultado1 = Math.pow(num1, 2) + Math.pow(num2, 2)
+   let resultado = Math.sqrt(resultado1)
+   console.log(resultado) 
+}
+
+hipotenusa(4, 5)
