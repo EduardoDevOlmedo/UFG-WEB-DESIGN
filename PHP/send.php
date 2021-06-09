f48cecfc6817b6da1a03a41721bd68b4dc868ddb
Eduardo Olmedo <eduardolmedi00@gmail.com>
2021-06-08 18:31:19 -0600

    paga 1/9

--- PHP/send.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $destino = "eduardolmedi00@gmail.com";
-  $contenido = "De: YourDestiny\n"" Asunto: COMPRA\n"" Se registró su compra exitosamente.";
-  mail($destino, $contenido);
-  header("Location:HTML\agradecimiento.html");
-?>
